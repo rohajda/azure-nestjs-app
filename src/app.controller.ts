f9f308cfb4e337d456da33de2648c32c77b9a2af
Robert Ohajda <ohajda@emm.sk>
2024-06-24 19:23:45 +0200

    init

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,20 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Post } from "@nestjs/common";
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  private logger = new Logger('AppController');
+
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
+    this.logger.log('getHello invoked');
     return this.appService.getHello();
   }
 
   @Post()
   receiveEvent(@Body() event: any): void {
-    console.log(event);
+    this.logger.log('receiveEvent ' + JSON.stringify(event));
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger()
   const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
+  const port = 8080;
+  await app.listen(port);
+  logger.log(`Application running on port ${port}`);
 }
 bootstrap();
